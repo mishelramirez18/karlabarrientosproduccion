9f78791218f9bcc064b761f7f7a101633ed896da
karla <kmbarrientosb8664@estudiante.intellectus.edu.gt>
2023-03-20 11:53:07 -0600

    se agrego un nombre

---
+++ lab 2/karlita.py
@@ -0,0 +1,13 @@
+edad=18
+print (edad)
+nombre= "karla"
+print(nombre)
+pi=3.1416
+print(pi)
+van_a_ganar_el_anno = True
+print (van_a_ganar_el_anno)
+
+print(type(edad))
+print(type(nombre))
+print(type(pi))
+print(type(van_a_ganar_el_anno))
