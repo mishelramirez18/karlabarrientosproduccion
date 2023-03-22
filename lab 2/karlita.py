82fbe4f09dfad01e0dd023636ac6ea12e3e872ef
karla <kmbarrientosb8664@estudiante.intellectus.edu.gt>
2023-03-22 08:38:15 -0600

    se agrego un texto aletorio

--- lab 2/karlita.py
+++ lab 2/karlita.py
@@ -11,3 +11,29 @@ print(type(edad))
 print(type(nombre))
 print(type(pi))
 print(type(van_a_ganar_el_anno))
+
+MESES_DEL_AÑO=12
+print(MESES_DEL_AÑO)
+
+print (f"karla¨{nombre}mishel")
+#esta variable indica mi nombre
+'''escuchar musica 
+sobre todo el vallenato'''
+num1=3
+num2=3
+resultado=num1+num2
+print (resultado)
+
+num1=4
+num2=5
+resultado=num1+num2
+print (resultado)
+
+resultado2=num1- resultado
+print (resultado2)
+
+resultado3= resultado2 * num2
+print (resultado3)
+
+numero=56 % 10
+print(numero)
